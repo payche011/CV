a08587e77a868453e15c854dae073d4df660f898
Pavle Marinkovic <payche255@gmail.com>
2022-02-25 09:49:02 +0100

    25/02/2022

--- pavle.js
+++ pavle.js
@@ -1,15 +1,1 @@
-const modalWrapper = document.querySelector(".modal-wrapper");
-const modalClose = document.querySelector(".modal-close");
-const body = document.querySelector("body");
-
-body.addEventListener("copy", () => {
-  modalWrapper.style.display = "block";
-});
-
-modalClose.addEventListener("click", () => {
-  modalWrapper.style.display = "none";
-});
-
-modalWrapper.addEventListener("click", () => {
-  modalWrapper.style.display = "none";
-});
+//
